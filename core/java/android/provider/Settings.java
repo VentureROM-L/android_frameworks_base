b453da3c17de0c99fee72c03c6dc221a02ee2093
Raj Yengisetty <rajesh@cyngn.com>
2014-11-14 09:44:50 -0800

    Protected apps
    Change-Id: Ic690387cd21fdfa09ef5fb19bd3de9305050cf6e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5287,6 +5287,11 @@ public final class Settings {
          */
         public static final String SLEEP_TIMEOUT = "sleep_timeout";
 
+         /** Protected Components
+         * @hide
+         */
+        public static final String PROTECTED_COMPONENTS = "protected_components";
+
         /**
          * This are the settings to be backed up.
          *
